9ffbeeee415aec34c67cd2f43dcfea8da476fb73
Rostik Bohomolov <ganster4656go@gmail.com>
2024-03-05 16:20:51 +0100

    Site

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpen.forEach(btn => {
+    btn.addEventListener('click', () => {
+      toggleModal();
+    });
+  });
